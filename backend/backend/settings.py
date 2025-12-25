2cea4b8bf5a5d81e8e661ca1095c6e6ef5bbac0a
Adithyan M <Adithyanm2327@gmail.com>
2025-12-25 17:06:07 +0530

    Switch email backend to SendGrid

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,13 +122,15 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # ===== EMAIL CONFIGURATION =====
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = "lakshyasenaacademy@gmail.com"
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
